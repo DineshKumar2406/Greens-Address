ad7236193e84abc55553a16bcfafe596fc41b14f
DineshKumar2406 <dineshkumar.st96@gmail.com>
2021-07-03 11:59:12 +0530

    my first push

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,35 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		System.out.println("greensOmr");
+		}
+	private void greensAdayar() {
+		System.out.println("greensAdayar");
+
+	}
+	private void greensTambaram() {
+	  System.out.println("greensTambaram");
+	  
+
+	}
+	
+	private void greensVelacherry() {
+	System.out.println("greensVelacherry");
+
+	}
+	
+private void greensAnnaNagar() {
+System.out.println("greensAnnaNagar");
+}
+public static void main(String[] args) {
+	GreensTech a = new GreensTech();
+	a.greensOmr();
+	a.greensAdayar();
+	a.greensTambaram();
+	a.greensVelacherry();
+	a.greensAnnaNagar();
+	
+}
+
+}
